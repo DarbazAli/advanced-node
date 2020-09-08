19617fa63f5891e5c63be92e6faca5de890e90c0
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-08 11:04:57 +0300

    setup passport

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const epxress = require('express');
+const passport = require('passport');
+const session = require('express-session');
+require('dotenv').config();
 
 const app = epxress();
 
 app.listen(3000, () => console.log("Listenting on 3000"))
 
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 3600000
+}))
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res) => {
+
+
+app.get('/', (req, res, next) => {
     res.render('index', {
         title: 'Advanced Node and Express',
         message: 'Please login'
