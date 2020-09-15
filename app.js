59d811acc652fb6289cbb685390af8dcef30bf7a
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-15 14:06:50 +0300

    logout user

--- app.js
+++ app.js
@@ -76,6 +76,22 @@ DATABASE( async client => {
                 successRedirect: '/profile'
             }))
 
+    /*======================================================
+        9) LOGOUT USER
+    =======================================================*/
+    app
+        .route('/logout')
+        .get((req, res) => {
+            req.logout();
+            res.redirect('/');
+        })
+    
+    app.use((req, res, next) => {
+        res
+            .status(404)
+            .type('text')
+            .send('Not found!')
+    })
 
     /*======================================================
     4) SERIALIZE USER OBJECT
