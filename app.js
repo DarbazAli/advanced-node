d210f085e5dba71607b14725d3663392f9215097
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-15 10:41:22 +0300

    setup passport & session

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const log = console.log;
 const PORT = process.env.PORT || 3000
 
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
 app.listen(PORT, log(`Server running on port ${PORT}`))
 
 
+/*======================================================
+    3) SETUP PASSPORT & SESSI0N
+=======================================================*/
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*======================================================
     1) SETUP TEMPLATE ENGINE
