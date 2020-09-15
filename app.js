8a67b66e8d651398a1a4f168bda2a9de04885a92
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-15 10:23:31 +0300

    setup template engine

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ const express = require('express');
 const app = express();
 
 app.listen(PORT, log(`Server running on port ${PORT}`))
+
+
+
+/*======================================================
+    1) SETUP TEMPLATE ENGINE
+=======================================================*/
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+
+
+
+app
+    .route('/')
+    .get((req, res) => res.render('index'))
