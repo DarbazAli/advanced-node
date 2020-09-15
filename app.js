3f8bd25c6e7318a6eaaa60ab31e2fc950ad5417c
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-15 10:17:03 +0300

    reprogramm

--- app.js
+++ app.js
@@ -1,47 +1,10 @@
-const epxress = require('express');
-const passport = require('passport');
-const session = require('express-session');
-require('dotenv').config();
+'use strict';
+console.clear();
+const log = console.log;
+const PORT = process.env.PORT || 3000
 
-const db = require('mongodb');
+const express = require('express');
 
+const app = express();
 
-const app = epxress();
-
-app.listen(3000, () => console.log("Listenting on 3000"))
-
-
-// setup session middleware
-app.use(session({
-    secret: process.env.SESSION_SECRET, 
-    resave: true,
-    saveUninitialized: true,
-    maxAge: 3600000
-}))
-// init passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-    done(null, user._id);
-})
-
-passport.deserializeUser((id, done) => {
-    db.collection('users')
-        .findOne({_id: new db.ObjectID(id)}, (err, doc) => {
-            done(null, doc);
-        })
-})
-
-
-app.set('view engine', 'pug');
-app.set('views', 'views');
-
-
-
-app.get('/', (req, res, next) => {
-    res.render('index', {
-        title: 'Advanced Node and Express',
-        message: 'Please login'
-    })
-})
+app.listen(PORT, log(`Server running on port ${PORT}`))
